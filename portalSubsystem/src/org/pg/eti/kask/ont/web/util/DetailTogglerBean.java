b8c5f03357c87e0f014d86812bcb6ce53ce7376c
Piotr Orlowski <106386op>
2009-05-05 10:24:19 +0000

    [update] sprzątanie portal subsystem [add] pliki graficzne [update] sws

--- portalSubsystem/src/org/pg/eti/kask/ont/web/util/DetailTogglerBean.java
+++
@@ -1,7 +0,0 @@
-package org.pg.eti.kask.ont.web.util;
-
-//import org.apache.myfaces.component.html.ext.HtmlDataTable;
-
-public class DetailTogglerBean /*extends HtmlDataTable*/ {
-
-}
--- portalSubsystem/web/index.jsp
+++
@@ -1,3 +0,0 @@
-<% 
- response.sendRedirect("pages/home.jsf");
-%>
