13d27c6a3f82b7c1df714318b5ebc63b8227ed63
Radoslaw Kleczkowski <106317kr>
2009-06-02 09:54:40 +0000

    [update] SOVA.

--- SOVA/src/org/eti/kask/sova/demo1/Demo.java
+++ SOVA/src/org/eti/kask/sova/demo1/Demo.java
@@ -1,3 +1,62 @@
+<<<<<<< .mine
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.eti.kask.sova.demo1;
+
+import java.net.URI;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JFrame;
+import org.eti.kask.sova.utils.Debug;
+import org.eti.kask.sova.visualization.OVDisplay;
+import org.semanticweb.owl.apibinding.OWLManager;
+import org.semanticweb.owl.model.OWLOntologyCreationException;
+import org.semanticweb.owl.model.OWLOntologyManager;
+
+/**
+ *
+ * @author infinity
+ */
+public class Demo
+{
+
+	public static void main(String[] args)
+	{
+		Debug.setStream(System.out);
+
+		OVDisplay display = new OVDisplay();
+		display.setSize(720, 500); // set display size
+		// zoom with vertical right-drag
+
+		OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+		// We load an ontology from a physical URI - in this case we'll load the pizza
+		// ontology.
+		URI physicalURI = URI.create("file:/home/infinity/Projekty/svn/ocs_visualisation/onot-test.owl");
+		try {
+			// Now ask the manager to load the ontology
+			display.generateGraphFromOWl(manager.loadOntologyFromPhysicalURI(physicalURI));
+		} catch (OWLOntologyCreationException ex) {
+			Logger.getLogger(Demo.class.getName()).log(Level.SEVERE, null, ex);
+		}
+
+
+
+
+// create a new window to hold the visualization
+		JFrame frame = new JFrame("SOVA prefuse usage demo");
+// ensure application exits when window is closed
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(display);
+		frame.pack();           // layout components in window
+		frame.setVisible(true); // show the window
+
+		
+
+	}
+}
+=======
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -51,3 +110,4 @@ public class Demo
 
 	}
 }
+>>>>>>> .r120
