3eab5c56a4a115faf6de9f312a9790dc78fdc2f6
Piotr Kunowski <106345kp>
2010-07-01 22:59:01 +0000

    wprowadzenie ogólnej obsługi reasonera, teraz resoner jest pobierany z protege'a

--- pluginProtegeSova/src/org/pg/eti/kask/ont/pluginSova/HierarchyTreeVis.java
+++ pluginProtegeSova/src/org/pg/eti/kask/ont/pluginSova/HierarchyTreeVis.java
@@ -1,14 +1,22 @@
 package org.pg.eti.kask.ont.pluginSova;
 
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JPanel;
+
+import org.pg.eti.kask.sova.utils.ReasonerLoader;
 import org.pg.eti.kask.sova.visualization.OVDisplay;
 import org.protege.editor.owl.ui.view.AbstractOWLViewComponent;
 
 public class HierarchyTreeVis extends AbstractOWLViewComponent {
-
+    private  JPanel leftPanel = null, rightPanel = null;
 	private  OVDisplay display;
-	private JPanel panel = null;
 	@Override
 	protected void disposeOWLView() {
 		display.removeDisplayVis();	
@@ -16,22 +24,57 @@ public class HierarchyTreeVis extends AbstractOWLViewComponent {
 
 	@Override
 	protected void initialiseOWLView() throws Exception {
-		if (display == null){
-    		display = new OVDisplay();
-    		display.setSize(800, 600);
+		ReasonerLoader.getInstance().setReasoner(getOWLModelManager().getReasoner());
+		System.out.println("załadowanie reasonera: "+getOWLModelManager().getReasoner() );
     	
-    		
+    	if (display == null){
+    		display = new OVDisplay();
+    		display.setSize(1000, 900);
         }
     	display.generateTreeFromOWl(getOWLModelManager().getActiveOntology());
-    	this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
-    	if (panel == null){
-	    	panel = new JPanel();
-			panel.add(display);
-			panel.setSize(800, 600);
-			panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-			this.add(panel);
-			this.repaint();
+        this.setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+        display.setSize(1000, 900);
+        if (leftPanel == null){
+        	initLeftPanel();
+        	this.add(leftPanel);
         }
+        if (rightPanel==null){
+        	initRightPanel();
+        	this.add(rightPanel);
+        }
+    	
+    	
 	}
+	
+	private void initRightPanel() {
+		rightPanel = new JPanel();
+		rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.Y_AXIS));
+		ImagePanel icon = new ImagePanel(new ImageIcon(getClass().getResource(
+				"/img/SOVA.png")).getImage());
+		rightPanel.add(icon);
+		JPanel buttonPanel = new JPanel(new GridLayout(8, 1));
+		JButton but2 = new JButton("Reset");
+		but2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				display.removeDisplayVis();
+				ReasonerLoader.getInstance().setReasoner(getOWLModelManager().getReasoner());
+				display.generateTreeFromOWl(getOWLModelManager().getActiveOntology());
+				
+			}
+		});
+		but2.setSize(100, 80);
+		but2.setToolTipText("Reload ontology");
+		buttonPanel.add(but2);
+
+		rightPanel.add(buttonPanel);
+		rightPanel.setPreferredSize(new Dimension(120, Integer.MAX_VALUE));
+		rightPanel.setMaximumSize(new Dimension(140, Integer.MAX_VALUE));
+		rightPanel.setMinimumSize(new Dimension(100, Integer.MAX_VALUE));
+	}
+    private void initLeftPanel(){
+        leftPanel = new JPanel();
+		leftPanel.add(display);
+		leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
+    }
 
 }
