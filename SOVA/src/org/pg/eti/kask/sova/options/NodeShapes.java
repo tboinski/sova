e234ceff3004fb38083d3dd5f90e826ba774468e
Tomasz Maria Boinski <tobo@eti.pg.gda.pl>
2011-11-08 09:10:29 +0000

    dodane informacje o licencji

--- SOVA/src/org/pg/eti/kask/sova/options/NodeShapes.java
+++ SOVA/src/org/pg/eti/kask/sova/options/NodeShapes.java
@@ -1,3 +1,25 @@
+/*
+ *
+ * Copyright (c) 2010 Gdańsk University of Technology
+ * Copyright (c) 2010 Kunowski Piotr
+ * Copyright (c) 2010 Jaworska Anna
+ * Copyright (c) 2010 Kleczkowski Radosław
+ * Copyright (c) 2010 Orłowski Piotr
+ *
+ * This file is part of OCS.  OCS is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU General Public
+ * License as published by the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package org.pg.eti.kask.sova.options;
 
 /**
