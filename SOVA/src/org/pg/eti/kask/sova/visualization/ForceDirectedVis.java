2630cbfe3580220c9cd8c23f5a291fc01676b3ef
Tomasz Maria Boinski <tobo@eti.pg.gda.pl>
2012-01-12 17:04:45 +0000

    licence fix

--- SOVA/src/org/pg/eti/kask/sova/visualization/ForceDirectedVis.java
+++ SOVA/src/org/pg/eti/kask/sova/visualization/ForceDirectedVis.java
@@ -6,7 +6,7 @@
  * Copyright (c) 2010 Kleczkowski Radosław
  * Copyright (c) 2010 Orłowski Piotr
  *
- * This file is part of OCS.  OCS is free software: you can
+ * This file is part of SOVA.  SOVA is free software: you can
  * redistribute it and/or modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation, version 3.
  *
