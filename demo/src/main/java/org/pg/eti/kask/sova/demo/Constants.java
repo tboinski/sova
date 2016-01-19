ffe74e8f0da50b75c05e482be9d7f293a8d6e750
Wojciech Zielonka <g36@wp.pl>
2016-01-19 21:11:52 +0100

    Maven tasks

---
+++ demo/src/main/java/org/pg/eti/kask/sova/demo/Constants.java
@@ -0,0 +1,39 @@
+/*
+ *
+ * Copyright (c) 2010 Gdańsk University of Technology
+ * Copyright (c) 2010 Kunowski Piotr
+ * Copyright (c) 2010 Jaworska Anna
+ * Copyright (c) 2010 Kleczkowski Radosław
+ * Copyright (c) 2010 Orłowski Piotr
+ *
+ * This file is part of SOVA.  SOVA is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with
+ * this program; If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package org.pg.eti.kask.sova.demo;
+
+/**
+ *
+ * @author Piotr Kunowski
+ */
+public class Constants {
+//     public static final String ONTO_TEST_DIRECTORY = "file:/home/boing/index.rdf";
+    public static final String ONTO_TEST_DIRECTORY = "X:\\Repozytoria\\sova\\doc\\OWL\\onot-test.owl";
+//    public static final String ONTO_TEST_DIRECTORY = "file:/home/boing/ontologies/ontC.owl";//risk_ENISA.owl";
+//    public static final String ONTO_TEST_DIRECTORY = "file:/home/boing/ontologies/moduły/moduł_podstawowy/RiskCoreConcepts.owl";
+//    public static final String ONTO_TEST_DIRECTORY = "file:/home/boing/ontologies/dziedzinowe/testy/00-wyjsciowa.rdf";
+
+    public static final String DEBUG_FILE = "myfile.log";
+//     public static final String PROPERTIES="/home/piotr29/STUDIA/OCS/SOVA/src/visualization.properties";
+//    public static final String PROPERTIES = "/home/boing/tools/NetBeansProjects/sova/SOVA/src/visualization.properties";
+     public static final String PROPERTIES = "X:\\Repozytoria\\sova\\SOVA\\src\\visualization.properties";
+}
---
+++ demo/src/main/java/org/pg/eti/kask/sova/demo/ImagePanel.java
@@ -0,0 +1,60 @@
+/*
+ *
+ * Copyright (c) 2010 Gdańsk University of Technology
+ * Copyright (c) 2010 Kunowski Piotr
+ * Copyright (c) 2010 Jaworska Anna
+ * Copyright (c) 2010 Kleczkowski Radosław
+ * Copyright (c) 2010 Orłowski Piotr
+ *
+ * This file is part of SOVA.  SOVA is free software: you can
+ * redistribute it and/or modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License along with
+ * this program; If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package org.pg.eti.kask.sova.demo;
+
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Image;
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+/**
+ * Panel z grafiką
+ * @author Piotr Kunowski
+ *
+ */
+class ImagePanel extends JPanel {
+
+	  private Image img;
+
+	  public ImagePanel(String img) {
+	    this(new ImageIcon(img).getImage());
+	  }
+
+	  public ImagePanel(Image img) {
+	    this.img = img;
+	    if (img==null || img.getWidth(null) <=0 || img.getHeight(null)<=0 ){
+	    	System.out.println("Brak grafiki!!!");
+	    }
+	    Dimension size = new Dimension(img.getWidth(null), img.getHeight(null));
+	    setPreferredSize(size);
+	    setMinimumSize(size);
+	    setMaximumSize(size);
+	    setSize(size);
+	    setLayout(null);
+	  }
+
+	  public void paintComponent(Graphics g) {
+	    g.drawImage(img, 0, 0, null);
+	  }
+
+	}
